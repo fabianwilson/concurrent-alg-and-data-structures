bfdeb38ad1f90138aa714bc48312e061f7a8a970
fabianwilson <fabian.wilson.6003@student.uu.se>
2026-01-08 14:30:12 +0100

    r

--- lab03/src/treiber_stack.hpp
+++ lab03/src/treiber_stack.hpp
@@ -3,14 +3,14 @@
 #include "adt.hpp"
 
 struct TreiberStackNode {
-    // A01: You can add or remove fields as needed.
+    // A01
     int value;
     TreiberStackNode* next = nullptr;
 };
 
 class TreiberStack: public Stack {
 private:
-    // A01: You can add or remove fields as needed.
+    // A01
     std::atomic<TreiberStackNode*> top;
 
     EventMonitor<TreiberStack, StdStack, StackOperator>* monitor;
@@ -19,12 +19,12 @@ public:
     TreiberStack(EventMonitor<TreiberStack, StdStack, StackOperator>* monitor)
         : monitor(monitor)
     {
-        // A01: Initiate the internal state
+        // A01
         this->top.store(nullptr);
     }
 
     ~TreiberStack() {
-        // A01: Cleanup any memory that was allocated
+        // A01
         TreiberStackNode* cur = this->top.load();
         while (cur) {
             TreiberStackNode* nxt = cur->next;
@@ -35,61 +35,81 @@ public:
 
     int push(int value) override {
         int result = true;
-        // A01: Add code to insert the element at the top of the stack.
+        // A01
         TreiberStackNode* node = new TreiberStackNode();
         node->value = value;
 
-        {
-            std::lock_guard<std::mutex> guard(this->cas_lock);
-            TreiberStackNode* old = this->top.load();
-            node->next = old;
-            this->top.store(node);
+        while (true) {
+            TreiberStackNode* t = this->top.load();
+            node->next = t;
 
-            this->monitor->add(StackEvent(StackOperator::StackPush, value, result));
+           
+            this->cas_lock.lock();
+            
+            if (this->top.compare_exchange_strong(t, node)) {
+                // linearization point: successful CAS
+                this->monitor->add(StackEvent(StackOperator::StackPush, value, result));
+                this->cas_lock.unlock();
+                return result;
+            }
+            // CAS failed, release lock and retry
+            this->cas_lock.unlock();
         }
-
-        return result;
     }
 
     int pop() override {
         int result = EMPTY_STACK_VALUE;
-        // A01: Add code to pop the element at the top of the stack.
-        TreiberStackNode* popped = nullptr;
-        {
-            std::lock_guard<std::mutex> guard(this->cas_lock);
-            TreiberStackNode* cur = this->top.load();
-            if (cur == nullptr) {
-                result = EMPTY_STACK_VALUE;
-            } else {
-                popped = cur;
-                result = cur->value;
-                this->top.store(cur->next);
+        // A01
+        while (true) {
+            TreiberStackNode* t = this->top.load();
+            if (t == nullptr) {
+                this->cas_lock.lock();
+                if (this->top.load() == nullptr) {
+                    this->monitor->add(StackEvent(StackOperator::StackPop, NO_ARGUMENT_VALUE, EMPTY_STACK_VALUE));
+                    this->cas_lock.unlock();
+                    return EMPTY_STACK_VALUE;
+                }
+                this->cas_lock.unlock();
+                continue;
             }
 
-            this->monitor->add(StackEvent(StackOperator::StackPop, NO_ARGUMENT_VALUE, result));
+            this->cas_lock.lock();
+            if (this->top.compare_exchange_strong(t, t->next)) {
+                result = t->value;
+                this->monitor->add(StackEvent(StackOperator::StackPop, NO_ARGUMENT_VALUE, result));
+                this->cas_lock.unlock();
+                // delete t;
+                return result;
+            }
+            this->cas_lock.unlock();
         }
-
-        if (popped) delete popped; // no memory leak, not really needed cuz memleaks are allowed in this assignment 
-        return result;
     }
 
     int size() override {
         int result = 0;
-        // A01: Add code to get the size of the stack.
-        {
-            std::lock_guard<std::mutex> guard(this->cas_lock);
-            TreiberStackNode* cur = this->top.load();
-            while (cur) {
-                result++;
-                cur = cur->next;
-            }
-
-            this->monitor->add(StackEvent(StackOperator::StackSize, NO_ARGUMENT_VALUE, result));
+        // A01
+        this->cas_lock.lock();
+        TreiberStackNode* cur = this->top.load();
+        while (cur) {
+            result++;
+            cur = cur->next;
         }
+        this->monitor->add(StackEvent(StackOperator::StackSize, NO_ARGUMENT_VALUE, result));
+        this->cas_lock.unlock();
         return result;
     }
 
     void print_state() override {
-        std::cout << "TreiberStack { ... }";
+        std::lock_guard<std::mutex> guard(this->cas_lock);
+        std::cout << "TreiberStack {";
+        TreiberStackNode* cur = this->top.load();
+        bool first = true;
+        while (cur) {
+            if (!first) std::cout << ", ";
+            std::cout << cur->value;
+            first = false;
+            cur = cur->next;
+        }
+        std::cout << "}";
     }
 };
